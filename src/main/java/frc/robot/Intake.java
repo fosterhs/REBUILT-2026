f4b014d146a215d82cd8f2575a076ba107b355ca
Skrat71 <145165749+Skrat71@users.noreply.github.com>
2026-02-05 15:07:44 -0800

    Added Motors to index class
    aa

--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -1,8 +1,26 @@
 package frc.robot;
 
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Intake {
   public enum Mode {HOME, LEFT, RIGHT, STOW} //
-  public Mode currMode = Mode.HOME; //
+  public Mode currMode = Mode.HOME; //by default, the intake starts in the home position
+
+  private final CANBus canivore = new CANBus("canivore");
+  private final TalonFX indexLeftRollerMotor = new TalonFX(0, canivore);
+  private final TalonFX indexRightRollerMotor = new TalonFX(1, canivore);
+  private final TalonFX intakeLeftArmMotor  = new TalonFX(2, canivore);
+  private final TalonFX intakeRightArmMotor = new TalonFX(3, canivore);
+
+  private final VelocityVoltage indexMotorVelocityRequest = new VelocityVoltage(0.0).withEnableFOC(true);
+
   public double desiredLeftArmPosition = 0.0; //
   public double desiredLeftRollerVelocity = 0.0; //
   public double desiredRightArmPosition = 0.0; //
@@ -10,12 +28,15 @@ public class Intake {
 
   //
   public Intake() {
-
+    configMotor(indexLeftRollerMotor, false);
+    configMotor(indexRightRollerMotor, false);
+    configMotor(intakeLeftArmMotor, false);
+    configMotor(intakeRightArmMotor, false);
   }
 
   //
   public void init() {
-
+    currMode = Mode.HOME;
   }
 
   //
@@ -34,7 +55,7 @@ public class Intake {
       break;
 
       case STOW:
-
+        
       break;
     }
   }
@@ -121,11 +142,28 @@ public class Intake {
 
   // Returns true or false based on whether the left arm, left roller, right arm, and right roller are all in their correct positions/velocities.
   public boolean isReady() {
-    return true;
+    return leftArmInPosition() && leftRollerAtSpeed() && rightArmInPosition() && rightRollerAtSpeed();
   }
 
   //
   public void updateDash() {
+    SmartDashboard.putString("Intake State", currMode.toString());
+  }
 
+  // Configs the motor settings and PID
+  private void configMotor(TalonFX motor, boolean invert) {
+    TalonFXConfiguration motorConfigs = new TalonFXConfiguration();
+
+    motorConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    motorConfigs.MotorOutput.Inverted = invert ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+
+    // VelocityVoltage closed-loop control configuration.
+    motorConfigs.Slot0.kP = 0.25; // Units: volts per 1 motor rotation per second of error.
+    motorConfigs.Slot0.kI = 0.5; // Units: volts per 1 motor rotation per second * 1 second of error.
+    motorConfigs.Slot0.kD = 0.0; // Units: volts per 1 motor rotation per second / 1 second of error.
+    motorConfigs.Slot0.kV = 0.12; // The amount of voltage required to create 1 motor rotation per second.
+    motorConfigs.Slot0.kS = 0.16; // The amount of voltage required to barely overcome static friction.
+
+    motor.getConfigurator().apply(motorConfigs, 0.03);
   }
 }
