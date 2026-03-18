898eb6ddd8d0b01abedd910eb7520bf18d3eeaa0
kpratap1 <121640740+kpratap1@users.noreply.github.com>
2026-03-17 19:16:29 -0700

    update cal

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -829,8 +829,8 @@ public class Robot extends TimedRobot {
   // This method calculates the position the hood needs to be at to shoot accurately based on the distance to the target. It uses a calibration array to return hood position values based on distance to the target.
   private double[] scoringHoodCalibrationDistances = {1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 5.0, 6.0}; 
   private double[] scoringHoodCalibrationValues = {0.02, 0.035, 0.0545, 0.0575, 0.062, 0.065, 0.068, 0.06895, 0.06}; 
-  private double[] passingHoodCalibrationDistances = {4.0, 6.0, 8.0, 10.0, 12.0};
-  private double[] passingHoodCalibrationValues = {0.06, 0.066, 0.092, 0.0105, 0.012};
+  private double[] passingHoodCalibrationDistances = {4.0, 6.0, 8.0, 10.0 };
+  private double[] passingHoodCalibrationValues = {0.06, 0.066, 0.092, 0.115};
   private double calcHoodPosition() {
     if (isScoring) {
       return interpolate(distanceToTarget, scoringHoodCalibrationDistances, scoringHoodCalibrationValues);
